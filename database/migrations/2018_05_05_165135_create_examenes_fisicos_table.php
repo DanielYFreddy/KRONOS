2d1841b9d9c84f91c9e39045df0361f93fe83a23
Daniel <dgerardo94@gmail.com>
2018-05-05 11:09:53 -0600

    Creacion migracion examenesfisicos

---
+++ database/migrations/2018_05_05_165135_create_examenes_fisicos_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamenesFisicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examenesFisicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('paciente_id')->unsigned();
+            $table->string('presionArterial');
+            $table->string('temperatura');
+            $table->string('frecuenciaCardiaca');
+            $table->string('hidratacion');
+            $table->string('peso');
+            $table->string('orl');
+            $table->string('cuello');
+            $table->string('torax');
+            $table->string('abdomen');
+            $table->string('extremidades');
+            $table->string('neurologico');
+            $table->date('fecha');
+
+            //Llaves Foraneas
+            $table->foreign('paciente_id')->references('id')->on('pacientes');
+
+            $table->timestamps();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examenesFisicos');
+    }
+}
